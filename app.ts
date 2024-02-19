4508ba800491d4b96eddcfb01df18d0a1ca3a7d2
VenturingForth <opilapro@gmail.com>
2024-02-19 14:56:48 +0000

    imported API router into app.js

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const apiRouter = require("./");
+const apiRouter = require("./routes/api-router");
 
 /* needed later
 
 app.use(cors())
 app.use(express.json())
 */
+
+app.use("/api", apiRouter);
 
 module.exports = app;
