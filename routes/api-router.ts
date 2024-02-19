3b280d45e65caa7a9d9991bf49d6572063e080e0
VenturingForth <opilapro@gmail.com>
2024-02-19 14:52:38 +0000

    added usersRouter

--- routes/api-router.ts
+++ routes/api-router.ts
@@ -1,4 +1,6 @@
 const apiRouter = require("express").Router();
+const endpointsRouter = require("./endpoints-router");
+const katasRouter = require("./katas-router");
 //Require in routers the come off of this point
 
 apiRouter.use("/", endpointsRouter);
@@ -9,3 +11,6 @@ apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/solutions", solutionsRouter)
 apiRouter.use("/test", testRouter) 
 */
+module.exports = {
+    apiRouter
+}
