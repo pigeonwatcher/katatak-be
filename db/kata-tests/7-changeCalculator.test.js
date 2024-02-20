28639c95f7ea27f65a5b35df9c7f7248b167e18d
VenturingForth <opilapro@gmail.com>
2024-02-20 16:40:44 +0000

    removed import statements and commented out test functions

--- db/kata-tests/7-changeCalculator.test.js
+++ db/kata-tests/7-changeCalculator.test.js
@@ -1,20 +1,18 @@
-const changeCalculator = require('../change-calculator/change-calculator.js')
-
-describe("changeCalculator", () => {
+// describe("changeCalculator", () => {
     
-    test("should return an object", () => {
-        expect(typeof changeCalculator(1)).toBe('object')
-    })
-    test("should return correct object when change is just a penny", () => {
-        expect(changeCalculator(1)).toEqual({'1p':1})
-    })
-    test("should return correct object when change is under 10p", () => {
-        expect(changeCalculator(8)).toEqual({'5p':1,'2p':1,'1p':1})
-    })
-     test("should return correct object when change is under a pound", () => {
-        expect(changeCalculator(13)).toEqual({'10p':1,'2p':1,'1p':1})
-     })
-    test("should return correct object when change is under five pounds", () => {
-        expect(changeCalculator(256)).toEqual({'£2':1,'50p':1,'5p':1,'1p':1})
-    })
-})
+//     test("should return an object", () => {
+//         expect(typeof changeCalculator(1)).toBe('object')
+//     })
+//     test("should return correct object when change is just a penny", () => {
+//         expect(changeCalculator(1)).toEqual({'1p':1})
+//     })
+//     test("should return correct object when change is under 10p", () => {
+//         expect(changeCalculator(8)).toEqual({'5p':1,'2p':1,'1p':1})
+//     })
+//      test("should return correct object when change is under a pound", () => {
+//         expect(changeCalculator(13)).toEqual({'10p':1,'2p':1,'1p':1})
+//      })
+//     test("should return correct object when change is under five pounds", () => {
+//         expect(changeCalculator(256)).toEqual({'£2':1,'50p':1,'5p':1,'1p':1})
+//     })
+// })
--- db/kata-tests/8-upperLowerCamelCase.test.js
+++ db/kata-tests/8-upperLowerCamelCase.test.js
@@ -1,63 +1,61 @@
-const sentenceToCamelCase = require('../sentence-to-camel-case.js');
+// describe("Sentence To Camel Case Testing", () => {
+//     test("Should return an empty string if given an empty string and boolean", () => {
+//         //arrange
+//         sentence = "";
+//         //act
+//         upperCase = sentenceToCamelCase(sentence, true);
+//         lowerCase = sentenceToCamelCase(sentence, false);
 
-describe("Sentence To Camel Case Testing", () => {
-    test("Should return an empty string if given an empty string and boolean", () => {
-        //arrange
-        sentence = "";
-        //act
-        upperCase = sentenceToCamelCase(sentence, true);
-        lowerCase = sentenceToCamelCase(sentence, false);
+//         //assert
+//         expect(upperCase).toBe("");
+//         expect(lowerCase).toBe("");
+//     })
+//     test("Should take a single word string and a boolean and convert to upper case camel if true and lower case camel if false", () => {
+//         //arrange
+//         sentence = "code";
 
-        //assert
-        expect(upperCase).toBe("");
-        expect(lowerCase).toBe("");
-    })
-    test("Should take a single word string and a boolean and convert to upper case camel if true and lower case camel if false", () => {
-        //arrange
-        sentence = "code";
+//         //act
+//         upperCase = sentenceToCamelCase(sentence, true);
+//         lowerCase = sentenceToCamelCase(sentence, false);
 
-        //act
-        upperCase = sentenceToCamelCase(sentence, true);
-        lowerCase = sentenceToCamelCase(sentence, false);
+//         //assert
+//         expect(upperCase).toBe("Code");
+//         expect(lowerCase).toBe("code");
+//     })
+//     test("Should take a multiple word string and a boolean and convert to upper case camel if true and lower case camel if false", () => {
+//         //arrange
+//         sentence = "code fries my little brain";
 
-        //assert
-        expect(upperCase).toBe("Code");
-        expect(lowerCase).toBe("code");
-    })
-    test("Should take a multiple word string and a boolean and convert to upper case camel if true and lower case camel if false", () => {
-        //arrange
-        sentence = "code fries my little brain";
+//         //act
+//         upperCase = sentenceToCamelCase(sentence, true);
+//         lowerCase = sentenceToCamelCase(sentence, false);
 
-        //act
-        upperCase = sentenceToCamelCase(sentence, true);
-        lowerCase = sentenceToCamelCase(sentence, false);
+//         //assert
+//         expect(upperCase).toBe("CodeFriesMyLittleBrain");
+//         expect(lowerCase).toBe("codeFriesMyLittleBrain");
+//     })
+//     test("Should take a multiple word string with mixed capitalisation and convert to upper camel case if true and lower cmael case if false", () => {
+//         //arrange
+//         sentence = "in the Ning Nang nong where The Cows go Bong";
 
-        //assert
-        expect(upperCase).toBe("CodeFriesMyLittleBrain");
-        expect(lowerCase).toBe("codeFriesMyLittleBrain");
-    })
-    test("Should take a multiple word string with mixed capitalisation and convert to upper camel case if true and lower cmael case if false", () => {
-        //arrange
-        sentence = "in the Ning Nang nong where The Cows go Bong";
+//         //act
+//         upperCase = sentenceToCamelCase(sentence, true);
+//         lowerCase = sentenceToCamelCase(sentence, false);
 
-        //act
-        upperCase = sentenceToCamelCase(sentence, true);
-        lowerCase = sentenceToCamelCase(sentence, false);
+//         //assert
+//         expect(upperCase).toBe("InTheNingNangNongWhereTheCowsGoBong");
+//         expect(lowerCase).toBe("inTheNingNangNongWhereTheCowsGoBong");
+//     })
+//     test("Should take a multiple word string with mixed casing throughout and return the correct case based on the boolean", () => {
+//         //arrange
+//         sentence = "anD thE MonkEyS Go JibbER JABber JOo";
 
-        //assert
-        expect(upperCase).toBe("InTheNingNangNongWhereTheCowsGoBong");
-        expect(lowerCase).toBe("inTheNingNangNongWhereTheCowsGoBong");
-    })
-    test("Should take a multiple word string with mixed casing throughout and return the correct case based on the boolean", () => {
-        //arrange
-        sentence = "anD thE MonkEyS Go JibbER JABber JOo";
+//         //act
+//         upperCase = sentenceToCamelCase(sentence, true);
+//         lowerCase = sentenceToCamelCase(sentence, false);
 
-        //act
-        upperCase = sentenceToCamelCase(sentence, true);
-        lowerCase = sentenceToCamelCase(sentence, false);
-
-        //assert
-        expect(upperCase).toBe("AndTheMonkeysGoJibberJabberJoo");
-        expect(lowerCase).toBe("andTheMonkeysGoJibberJabberJoo");
-    })
-})
+//         //assert
+//         expect(upperCase).toBe("AndTheMonkeysGoJibberJabberJoo");
+//         expect(lowerCase).toBe("andTheMonkeysGoJibberJabberJoo");
+//     })
+// })
