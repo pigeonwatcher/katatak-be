79a343055f7f7ba20ff87881cdecdfacdda1cbc9
VenturingForth <opilapro@gmail.com>
2024-02-20 16:19:20 +0000

    kata tests added to kata testing squite

---
+++ db/kata-tests/5-fizzBuzzMultiples.test.js
@@ -0,0 +1,39 @@
+const addFizzBuzzMultiples = require("../add-fizz-buzz-multiples.js");
+
+describe("Testing for FizzBuzz multiples of 3 and 5 below a certain number summed together", () => {
+    test("FizzBuzz multiples should sum to 0 if sent an integer of less than 4", () => {
+        expect(addFizzBuzzMultiples(1)).toBe(0);
+        expect(addFizzBuzzMultiples(2)).toBe(0);
+        expect(addFizzBuzzMultiples(3)).toBe(0);
+    })
+    test("FizzBuzz multiples should sum to 3 if sent an integer of at least 5", () => {
+        input = 5;
+        expectedOutput = 3;
+
+        expect(addFizzBuzzMultiples(input)).toBe(expectedOutput);
+    })
+    test("FizzBuzz multiples should sum to 8 if given an integer of 6", () => {
+        input = 6;
+        expectedOutput = 8;
+
+        expect(addFizzBuzzMultiples(input)).toBe(expectedOutput);
+    })
+    test("FizzBuzz multiples should sum to 23 if given an integer of 10", () => {
+        input = 10;
+        expectedOutput = 23;
+
+        expect(addFizzBuzzMultiples(input)).toBe(expectedOutput);
+    })
+    test("FizzBuzz multiples should sum to 33 if given an integer of 12", () => {
+        input = 12;
+        expectedOutput = 33;
+
+        expect(addFizzBuzzMultiples(input)).toBe(expectedOutput);
+    })
+    test("FizzBuzz multiples should sum to 2318 if given an integer of 100", () => {
+        input = 100;
+        expectedOutput = 2318;
+
+        expect(addFizzBuzzMultiples(input)).toBe(expectedOutput);
+    })
+})
