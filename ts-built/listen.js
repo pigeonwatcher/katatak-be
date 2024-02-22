273cd77bd239ac69e333c9406c290aa9bd4a49e7
VenturingForth <opilapro@gmail.com>
2024-02-22 10:38:58 +0000

    deleted old build

--- ts-built/listen.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const app = require("./app");
-const { PORT = 9090 } = process.env;
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
--- ts-built/routes/users-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const usersRouter = require("express").Router();
-const users_controllers_1 = require("../mvc/controllers/users.controllers");
-usersRouter.route("/").get(users_controllers_1.getUsers);
-usersRouter.route("/:user_id").get(users_controllers_1.getUserById);
-module.exports = {
-    usersRouter,
-};
